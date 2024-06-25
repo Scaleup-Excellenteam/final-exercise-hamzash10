ecee86c303c12155a0c70512adc68de7b5b1e24e
hamzash <hamzashaheen.HAS@gmail.com>
2024-06-25 15:48:18 +0300

    added a convenient CLI interface 'using argpars', made the default file to be the file im using Corona.pptx

--- json_handler.py
+++ json_handler.py
@@ -1,5 +1,17 @@
 import json
+import os
 
-def save_to_json(slides_summary, json_file):
+def save_to_json(slides_summary, pptx_filename):
+    """
+    Saves the slides summary to a json file using the basename of the pptx file.
+    :param slides_summary: a list of slides summary
+    :param pptx_filename: the filename of the pptx
+    """
+
+    # change filename from .pptx to .json
+    base_name = os.path.splitext(pptx_filename)[0]
+    json_file = base_name + '.json'
+
+    # save as json
     with open(json_file, 'w') as f:
         json.dump(slides_summary, f, indent=4)
--- main.py
+++ main.py
@@ -2,11 +2,20 @@ from presentation_reader import read_pptx
 import chatgpt_interaction as gpt
 from json_handler import save_to_json
 import asyncio
+import argparse
+
 
 async def main():
-    slides_content = read_pptx("Corona.pptx")
+    parser = argparse.ArgumentParser(description='process a powerpoint (.pptx) file and summarize its content using '
+                                                 'chatgpt')
+    parser.add_argument('input_pptx', type=str, nargs='?', default='Corona.pptx', help='path to the powerpoint file ('
+                                                                                       '.pptx) default is: Corona.pptx')
+
+    args = parser.parse_args()
+
+    slides_content = read_pptx(args.input_pptx)
     slides_summary = await gpt.process_slides(slides_content)
-    save_to_json(slides_summary, "Corona.json")
+    save_to_json(slides_summary, args.input_pptx)
 
 if __name__ == '__main__':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
