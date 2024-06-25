3f3073591dd496594f0f2f6cb0ce5821f8b9de31
hamzash <hamzashaheen.HAS@gmail.com>
2024-06-25 08:45:48 +0300

    added basic asyncious programming

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import Presentation as presentation
 import openAI as gpt
+import asyncio
 
-def main():
-    gpt.start_connection()
-
+async def main():
+    await gpt.start_connection()
 
 if __name__ == '__main__':
-    main()
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(main())
--- openAI.py
+++ openAI.py
@@ -1,18 +1,16 @@
-from openai import OpenAI
+from openai import AsyncOpenAI
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(
-    api_key=os.getenv('OPENAI_API_KEY')
-)
 
 
-def start_connection():
+async def start_connection():
+    client = AsyncOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
     message = "hi chat im communicating with you using python and openai, if the connection is successful please tell me"
     try:
-        chat_completion = client.chat.completions.create(
+        chat_completion = await client.chat.completions.create(
             messages=[
                 {
                     "role": "user",
@@ -22,5 +20,6 @@ def start_connection():
             model="gpt-3.5-turbo",
         )
         print(chat_completion.choices[0].message.content.strip())
+        return
     except Exception as e:
         print(f"An error occurred: {e}")
