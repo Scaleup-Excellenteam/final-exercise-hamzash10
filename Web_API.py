b20edc545c217a6bbfd0900c1a29abc54eb4033b
hamzash <hamzashaheen.HAS@gmail.com>
2024-06-27 09:34:54 +0300

    added a GET requist handling function i still havent tested it, beacuse of the workflow, testing and finalizing it will be with the Explainer branch

--- Web_API.py
+++ Web_API.py
@@ -6,10 +6,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
+OUTPUT_FOLDER = 'outputs'
 
 # make the uploads directory if it doesn't exist
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
+
+# make the outputs directory if it doesn't exist
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
 
 
 @app.route('/upload', methods=['POST'])
@@ -34,6 +39,53 @@ def upload_file():
     else:
         return jsonify({'error': 'Invalid file'}), 400
 
+
+@app.route('/status', methods=['GET'])
+def status():
+    """
+    Handling a GET request to get a file explanation using a UID
+    :return: JSON response with the all the details required
+    """
+    received_uid = request.args.get('uid')
+    if not received_uid:
+        # bad request
+        return jsonify({'error': 'No received UID'}), 400
+
+    # check the outputs folder
+    for full_filename in os.listdir(OUTPUT_FOLDER):
+        if received_uid in full_filename:
+            with open(os.path.join(OUTPUT_FOLDER, full_filename), 'r') as f:
+                explanation = f.read()
+
+            filename, timestamp, uid_and_extension = full_filename.split('_')
+
+            # ok
+            return jsonify({
+                'status': 'done',
+                'filename': filename,
+                'timestamp': timestamp,
+                'explanation': explanation
+            }), 200
+
+    # check the uploads folder
+    for full_filename in os.listdir(UPLOAD_FOLDER):
+        if received_uid in full_filename:
+            filename, timestamp, uid_and_extension = full_filename.split('_')
+            # Accepted ( the file exist but hasn't been processed yet )
+            return jsonify({
+                'status': 'pending',
+                'filename': filename,
+                'timestamp': timestamp,
+                'explanation': ''
+            }), 202
+
+    # Not Found
+    return jsonify({
+        'status': 'not found',
+        'filename': '',
+        'timestamp': '',
+        'explanation': ''
+    }), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
