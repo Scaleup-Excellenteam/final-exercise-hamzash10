9c07b76dbd7db99734660218eedf7e598e780718
hamzash <hamzashaheen.HAS@gmail.com>
2024-06-28 11:58:51 +0300

    added logging for the web api

--- Web_API.py
+++ Web_API.py
@@ -3,9 +3,25 @@ import time
 import uuid
 from Modules import UPLOAD_FOLDER, OUTPUT_FOLDER
 from flask import Flask, request, jsonify
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
 app = Flask(__name__)
 
+
+
+# check if the logging dir exist
+FLASK_LOG_DIR = os.path.join(os.path.dirname(__file__), 'logs', 'flask_server')
+if not os.path.exists(FLASK_LOG_DIR):
+    os.makedirs(FLASK_LOG_DIR)
+
+# configure logging
+log_file = os.path.join(FLASK_LOG_DIR, 'web_api.log')
+handler = TimedRotatingFileHandler(log_file, when='midnight', backupCount=5)
+formatter = logging.Formatter('%(levelname)s: %(message)s')
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.DEBUG)
 
 # make the uploads directory if it doesn't exist
 if not os.path.exists(UPLOAD_FOLDER):
@@ -23,10 +39,12 @@ def upload_file():
     :return: JSON response with the UID of the uploaded file, or an error message if the upload fails.
     """
     if 'file' not in request.files:
+        app.logger.error('No file found in the request')
         return jsonify({'error': 'No file found in the request'}), 400
 
     uploaded_file = request.files['file']
     if uploaded_file.filename == '':
+        app.logger.error('No file selected for uploading')
         return jsonify({'error': 'No file selected for uploading'}), 400
 
     if uploaded_file:
@@ -34,8 +52,10 @@ def upload_file():
         timestamp = int(time.time())
         new_filename = f"{uploaded_file.filename[:-5]}_{timestamp}_{generated_uid}.{uploaded_file.filename[-4:]}"
         uploaded_file.save(os.path.join(UPLOAD_FOLDER, new_filename))
+        app.logger.info(f"file uploaded successfully with UID {generated_uid}")
         return jsonify({'uid': generated_uid}), 200
     else:
+        app.logger.error("invalid file")
         return jsonify({'error': 'Invalid file'}), 400
 
 
@@ -48,6 +68,7 @@ def status():
     received_uid = request.args.get('uid')
     if not received_uid:
         # bad request
+        app.logger.error('No received UID')
         return jsonify({'error': 'No received UID'}), 400
 
     # check the outputs folder
@@ -58,6 +79,7 @@ def status():
 
             filename, timestamp, uid_and_extension = full_filename.split('_')
 
+            app.logger.info(f"file processed successfully")
             # ok
             return jsonify({
                 'status': 'done',
@@ -70,6 +92,7 @@ def status():
     for full_filename in os.listdir(UPLOAD_FOLDER):
         if received_uid in full_filename:
             filename, timestamp, uid_and_extension = full_filename.split('_')
+            app.logger.info("the file explanation is pending")
             # Accepted ( the file exist but hasn't been processed yet )
             return jsonify({
                 'status': 'pending',
@@ -78,6 +101,7 @@ def status():
                 'explanation': ''
             }), 202
 
+    app.logger.error("the file does not exist")
     # Not Found
     return jsonify({
         'status': 'not found',
